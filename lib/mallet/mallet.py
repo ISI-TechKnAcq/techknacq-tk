57b46a39b82d5bb2096b4132bcc33c55651d3603
Jonathan Gordon <jgordon@jonathangordon.org>
2016-05-04 15:05:38 -0700

    Add code for reading an existing topic model and import code from techknacq-main for generating topic co-occurrence files.

--- lib/mallet/mallet.py
+++ lib/mallet/mallet.py
@@ -9,7 +9,11 @@ import re
 import subprocess
 import multiprocessing as mp
 
+from numpy import zeros
+from itertools import combinations
+
 from t.lx import StopLexicon
+
 
 # Parameters
 
@@ -20,12 +24,15 @@ OPTIMIZE_INTERVAL = 10
 ###
 
 class Mallet:
-    def __init__(self, path, corpus, num_topics=200, bigrams=False,
-                 iters=1000):
+    def __init__(self, path, corpus=None, num_topics=200, bigrams=False,
+                 iters=1000, prefix=None):
         self.path = path
 
-        rand_prefix = hex(random.randint(0, 0xffffff))[2:] + '-'
-        self.prefix = os.path.join(tempfile.gettempdir(), rand_prefix)
+        if prefix:
+            self.prefix = prefix
+        else:
+            rand_prefix = hex(random.randint(0, 0xffffff))[2:] + '-'
+            self.prefix = os.path.join(tempfile.gettempdir(), rand_prefix)
 
         self.dtfile = self.prefix + 'composition.txt'
         self.wtfile = self.prefix + 'word-topic-counts.txt'
@@ -34,11 +41,17 @@ class Mallet:
         self.statefile = self.prefix + 'state.gz'
         self.cofile = self.prefix + 'co-occur.txt'
 
+        if os.path.exists(self.tkfile):
+            num_topics = len(open(self.tkfile).readlines())
+            print('Found model with', num_topics, 'topics.')
+
         self.topics = [[] for i in range(num_topics)]
 
-        self.read(corpus, bigrams)
-        self.train(num_topics, iters)
+        if not os.path.exists(self.wtfile) or not os.path.exists(self.dtfile):
+            self.read(corpus, bigrams)
+            self.train(num_topics, iters)
         self.load_wt()
+        self.load_dt()
 
 
     def read(self, corpus, bigrams):
@@ -49,7 +62,7 @@ class Mallet:
                '--output', self.prefix + 'corpus.mallet',
                '--remove-stopwords',
                '--extra-stopwords', stop.file,
-               '--token-regex', r'"[^ ]+"']
+               '--token-regex', '[^\\s]+']
 
         if bigrams:
             cmd += ['--keep-sequence-bigrams', '--gram-sizes 2']
@@ -78,10 +91,63 @@ class Mallet:
             sys.stderr.write('Mallet train-topics failed.\n')
             sys.exit(1)
 
+
     def load_wt(self):
+        print('Loading word-topic file.')
         for line in open(self.wtfile):
             tokens = line.strip().split()
             word = tokens[1]
             for c in tokens[2:]:
                 topic, count = c.split(':')
                 self.topics[int(topic)].append((word, float(count)))
+
+
+    def load_dt(self):
+        print('Loading document-topic composition file.')
+
+        num_topics = len(self.topics)
+        self.topic_doc = [[] for i in range(num_topics)]
+        self.co_occur = zeros((num_topics, num_topics), int)
+
+        # We need a cut-off for a topic to count as non-trivially occurring
+        # in a document, and this needs to vary depending on the number of
+        # topics. Based on experiments with 20 and 200 topic models, I chose
+        # the thresholds (20, 0.3) and (200, 0.1) and fit the line
+        #    y = -1/900*x + 290/900
+        # with a min of 0.01. This is a preliminary measure and should be
+        # adjusted for other corpora.
+        thresh = max((290.0 - num_topics)/900.0, 0.01)
+
+        for line in open(self.dtfile):
+            row = line.split('\t')
+            if row[0][0] == '#':
+                continue
+            if len(row) < 2:
+                print('Error with composition row', row, file=sys.stderr)
+                continue
+            m = re.search('([^/]+)\.(xml|txt)$', row[1])
+            if not m:
+                continue
+            base = m.group(1)
+
+            topics = [(int(a), float(b)) for (a, b) in
+                      zip(row[2::2], row[3::2])]
+
+            # Read into document topic breakdown information.
+            for topic_id, percent in topics:
+                self.topic_doc[topic_id].append((base, percent))
+
+            # Read into co-occurrence matrix.
+            filt_topics = [(a, b) for (a, b) in topics if b > thresh]
+            for topic_pair in combinations(filt_topics, 2):
+                i1 = topic_pair[0][0]
+                i2 = topic_pair[1][0]
+                # Symmetric matrix.
+                self.co_occur[i1][i2] += 1
+                self.co_occur[i2][i1] += 1
+
+        with open(self.cofile, 'w') as out:
+            for row in self.co_occur:
+                for c in row:
+                    out.write('%s ' % (c))
+                out.write('\n')
