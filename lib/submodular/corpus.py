238537da12cad69ac985dee0f2983d5972ada936
Kieu Binh <binhkt.vnu@gmail.com>
2019-01-12 18:43:54 +0700

    update lib/corpus

--- lib/submodular/corpus.py
+++ lib/submodular/corpus.py
@@ -1,13 +1,14 @@
 
 import os, glob
+import json
 
 class Corpus:
-    def __init__(self, path="../../data/acl/"):
+    def __init__(self, path="../../sample/"):
         print(path)
         for filename in glob.glob(os.path.join(path, '*.json')):
-            with open(filename, 'r') as f:
-                text = f.read()
+            with open(filename, 'r', encoding="utf-8") as fout:
+                jsondata = json.load(fout)
                 print(filename)
-                print(len(text))
+                print(jsondata['sections'][0])
 
 corpus = Corpus()
